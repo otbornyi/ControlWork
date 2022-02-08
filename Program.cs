cc54352671366d1e976b7bff78434e2441c8d38d
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-02-08 14:57:26 +0300

    initial input

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите слово или цифры: ");
+string test = Console.ReadLine();
+string[] temp = test.Split(new Char[] { ' ' });
+int K = temp.Length;
+
+void FillArray(string[] A)
+{
+    for (int i = 0; i < temp.Length; i++)
+    {
+        A[i] = temp[i];
+    }
+}
