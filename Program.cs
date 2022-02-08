960bbc55d64ff8dd0259a96ced06dc6d781ea6af
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-02-08 15:26:41 +0300

    simplifed the task

--- Program.cs
+++ Program.cs
@@ -3,26 +3,13 @@ string test = Console.ReadLine();
 string[] temp = test.Split(new Char[] { ' ' });
 int K = temp.Length;
 
-void FillArray(string[] A)
+
+
+for (int i = 0; i < temp.Length; i++)
 {
-    for (int i = 0; i < temp.Length; i++)
+    string Z = temp[i];
+    if (Z.Length < 4)
     {
-        A[i] = temp[i];
+        Console.Write($"{temp[i]} ");
     }
 }
-
-void Choise(string[] A)
-{
-    for (int i = 0; i < A.Length; i++)
-    {
-        string Z = A[i];
-        if (Z.Length < 4)
-        {
-            Console.Write($"{A[i]} ");
-        }
-    }
-}
-
-string[] A = new string[K];
-FillArray(A);
-Choise(A);
