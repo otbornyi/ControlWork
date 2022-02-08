69d7791c0b2a47fd92f7836f805e093ac2fde196
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-02-08 15:29:13 +0300

    little changes

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 ﻿Console.WriteLine("Введите слова либо цифры через пробел: ");
-string test = Console.ReadLine();
-string[] temp = test.Split(new Char[] { ' ' });
-int K = temp.Length;
+string text = Console.ReadLine();
+string[] WordsNumbers = text.Split(new Char[] { ' ' });
 
-
-
-for (int i = 0; i < temp.Length; i++)
+for (int i = 0; i < WordsNumbers.Length; i++)
 {
-    string Z = temp[i];
+    string Z = WordsNumbers[i];
     if (Z.Length < 4)
     {
-        Console.Write($"{temp[i]} ");
+        Console.Write($"{WordsNumbers[i]} ");
     }
 }
