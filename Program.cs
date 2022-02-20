ab56c24e9657ab6ad27512c6890c794f36921cc6
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-02-20 14:10:18 +0300

    made amendments and edit flowchart

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
 ﻿Console.WriteLine("Введите слова либо цифры через пробел: ");
 string text = Console.ReadLine();
-string[] WordsNumbers = text.Split(new Char[] { ' ' });
-string[] FinalArray = new string[0];
-
-for (int i = 0; i < WordsNumbers.Length; i++)
+string[] FirstArray = text.Split(new Char[] { ' ' });
+void FillFinnalArray(string[] Final)
 {
-    int j = 0;
-    if (WordsNumbers[i].Length < 4)
-    { 
-        Array.Resize(ref FinalArray, j + 1);
-        FinalArray[j] = WordsNumbers[i];
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if(FirstArray[i].Length < 4)
+        {
+            Final[i] = FirstArray[i];
+        }
     }
-    Console.Write(String.Join(" ", FinalArray));
 }
+
+void PrintFinalArray(string[] Final)
+{
+    for (int i = 0; i < Final.Length; i++)
+    {
+        Console.Write($"{Final[i]} ");
+    }
+}
+
+string[] Final = new string[FirstArray.Length];
+FillFinnalArray(Final);
+PrintFinalArray(Final);
